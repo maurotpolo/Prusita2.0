606a31e41b18f1f472c51570ce2ea81ddeacd4bc
maurotpolo <mauropolopna@gmail.com>
2021-09-07 16:39:56 -0300

    Ajuste PIDTEMP Extrusor

--- Marlin-2.0.x/Marlin/Configuration.h
+++ Marlin-2.0.x/Marlin/Configuration.h
@@ -601,9 +601,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  14.69
+    #define DEFAULT_Ki   1.06
+    #define DEFAULT_Kd 51.15
   #endif
 #endif // PIDTEMP
 
